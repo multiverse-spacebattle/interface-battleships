a3b2d521f073f03f56f5b58113a8a377038b4a91
remi-gai <56744348+remi-gai@users.noreply.github.com>
2022-07-05 21:15:35 -0400

    added test networks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import {useNetwork, ChainId, useChainId, useAddress, useDisconnect, useCoinbaseWallet } from '@thirdweb-dev/react';
+
+
 
 function App() {
   const address = useAddress();
-  const connectWithMetamask = useMetamask();
+  const connectWithCoinbaseWallet = useCoinbaseWallet();
   const disconnectWallet = useDisconnect();
+  const chainId = useChainId();
+
+  const [, switchNetwork] = useNetwork();
+
   return (
     <div>
       {address ? (
@@ -12,8 +18,18 @@ function App() {
           <p>Your address: {address}</p>
         </>
       ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
+        <button onClick={connectWithCoinbaseWallet}>Connect with Coinbase Wallet</button>
       )}
+    <button onClick={() => switchNetwork(ChainId.Mumbai)}>
+      Switch To Mumbai
+    </button>
+    <button onClick={() => switchNetwork(ChainId.FantomTestnet)}>
+      Switch To FantomTestnet
+    </button>
+    <button onClick={() => switchNetwork(ChainId.AvalancheFujiTestnet)}>
+      Switch To AvalancheFujiTestnet
+    </button>
+    <div>ChainId: {chainId}</div>
     </div>
   );
 }
