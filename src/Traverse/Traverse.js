3bb02b712ddd60c337655885f09cf0d67965f0ab
remi-gai <56744348+remi-gai@users.noreply.github.com>
2022-07-10 10:11:00 -0400

    added portal selection

--- src/Traverse/Traverse.js
+++ src/Traverse/Traverse.js
@@ -10,6 +10,7 @@ function Traverse({
 }) {
   const [userSpaceshipSelection, setUserSpaceshipSelection] = useState(null);
   const [userSpaceshipDetails, setUserSpaceshipDetails] = useState(null);
+  const [portal, setPortal] = useState(null);
 
   const getUserSpaceships = () => {
     if (chainId.id === 4002) {
@@ -89,23 +90,58 @@ function Traverse({
       <div className="w-full border border-black">
         <div>Travel from Ethereum to Avalanche Galaxy</div>
         <div className="flex flex-row w-full items-center justify-around">
-          <div className="border rounded-2xl h-24 w-24">
+          <div
+            className={
+              portal === "Ethereum"
+                ? "h-24 w-24 rounded-2xl border border-blue-400 border-4 cursor-pointer"
+                : "h-24 w-24 rounded-2xl cursor-pointer"
+            }
+            onClick={() => setPortal("Ethereum")}
+          >
             <img src="./portal1.png" className="rounded-2xl"></img>
             <div>Ethereum</div>
           </div>
-          <div className="h-24 w-24">
+          <div
+            className={
+              portal === "Polygon"
+                ? "h-24 w-24 rounded-2xl border border-blue-400 border-4 cursor-pointer"
+                : "h-24 w-24 rounded-2xl cursor-pointer"
+            }
+            onClick={() => setPortal("Polygon")}
+          >
             <img src="./portal2.png" className="rounded-2xl"></img>
             <div>Polygon</div>
           </div>
-          <div className="h-24 w-24 rounded-2xl">
+          <div
+            className={
+              portal === "Avalanche"
+                ? "h-24 w-24 rounded-2xl border border-blue-400 border-4 cursor-pointer"
+                : "h-24 w-24 rounded-2xl cursor-pointer"
+            }
+            onClick={() => setPortal("Avalanche")}
+          >
             <img src="./portal3.png" className="rounded-2xl"></img>
             <div>Avalanche</div>
           </div>
-          <div className="h-24 w-24 rounded-2xl">
+          <div
+            className={
+              portal === "Binance"
+                ? "h-24 w-24 rounded-2xl border border-blue-400 border-4 cursor-pointer"
+                : "h-24 w-24 rounded-2xl cursor-pointer"
+            }
+            onClick={() => setPortal("Binance")}
+          >
             <img src="./portal4.png" className="rounded-2xl"></img>
             <div>Binance</div>
           </div>
-          <div className="h-24 w-24 rounded-2xl">
+          <div
+            className={
+              portal === "Fantom"
+                ? "h-24 w-24 rounded-2xl border border-blue-400 border-4 cursor-pointer"
+                : "h-24 w-24 rounded-2xl cursor-pointer"
+            }
+            onClick={() => setPortal("Fantom")}
+          >
             <img src="./portal5.png" className="rounded-2xl"></img>
             <div>Fantom</div>
           </div>
